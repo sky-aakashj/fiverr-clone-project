8a93fb2eb96727c5282be5b150fb5c25a5ae7703
sky-aakashj <aakashsjaiswal303@gmail.com>
2024-08-14 22:01:57 +0530

    add trustedBy section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import './Home.scss'
-import Featured from '../../components/featured/Featured'
+import React from "react";
+import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 
 const Home = () => {
-    return (
-        <Featured/>
-    )
-}
+  return (
+    <>
+      <Featured />
+      <TrustedBy />
+    </>
+  );
+};
 
-export default Home
+export default Home;
