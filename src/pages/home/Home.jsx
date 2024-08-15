1b607269586f2ec03550b9b629add938070a6795
sky-aakashj <aakashsjaiswal303@gmail.com>
2024-08-15 18:29:52 +0530

    add why fiverr section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,10 +5,11 @@ import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/slide/Slide";
 import { cards } from "../../sliderData";
 import CatCard from "../../components/catCard/CatCard";
+import WhyFiverr from "../../components/whyFiverr/WhyFiverr";
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <Featured />
       <TrustedBy />
       <Slide slidesToShow={5} arrowsScroll={3}>
@@ -16,7 +17,8 @@ const Home = () => {
           <CatCard item={card} key={card.id} />
         ))}
       </Slide>
-    </>
+      <WhyFiverr />
+    </div>
   );
 };
 
